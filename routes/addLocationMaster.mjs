e990b2a31e0309d7a326d4a8ae078aee834d6630
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2024-06-19 12:04:01 +0530

    fixed bugs in LR

--- routes/addLocationMaster.mjs
+++ routes/addLocationMaster.mjs
@@ -1,27 +1,3 @@
-// import express from "express";
-// import locationMaster from "../models/master/locationMaster.mjs";
-
-// const router = express.Router();
-
-// router.post("/api/addLocationMaster", async (req, res) => {
-//   try {
-//     const { district, area, pincode } = req.body;
-
-//     const newLocationMaster = await locationMaster.create({
-//       district,
-//       pincode,
-//       area: [...area],
-//     });
-
-//     res.status(201).json(newLocationMaster);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// export default router;
-
 import express from "express";
 import LocationMaster from "../models/master/locationMaster.mjs";
 
@@ -29,7 +5,7 @@ const router = express.Router();
 
 router.post("/api/addLocationMaster", async (req, res) => {
   try {
-    const { district, area, pincode } = req.body;
+    const { location, district, area, pincode } = req.body;
 
     // Try to find a document with the given district
     let locationMaster = await LocationMaster.findOne({ district });
@@ -42,6 +18,7 @@ router.post("/api/addLocationMaster", async (req, res) => {
     } else {
       // If the document doesn't exist, create a new one
       locationMaster = await LocationMaster.create({
+        location,
         district,
         area: [{ area, pincode }],
       });
